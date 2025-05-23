7f0e20cf2371e5ad2e22e5aa6d0add307f770dd4
Diana Aguiñaga <dianacarolina.ag14@gmail.com>
2025-05-23 01:22:56 -0600

    simplified validation function, added convert to number, validation of numbers, show result and handleInput

--- coordenadas.js
+++ coordenadas.js
@@ -4,19 +4,48 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-const validCoordinates (lat,lon) => {
-    if (lat < -90 || lat > 90) {
-        return false;
-    }
-    if (lon < -180 || lon > 180) {
-        return false;
-    }
-    return true;
-    }
+//validación
+const validCoordinates = (lat, lon) => {
+  return lat >= -90 && lat <= 90 && lon >= -180 && lon <= 180;
+};
 
-    rl.question('Enter latitude: ', (lat) => {
-       console.log(`Latitude: ${lat}`); 
-       rl.close();
+const areValidNumbers = (lat, lon) => {
+  return !isNaN(lat) && !isNaN(lon);
+};
+
+// convierte input de readline a número
+const convertToNumber = (latInput, lonInput) => {
+  return {
+    lat: parseFloat(latInput),
+    lon: parseFloat(lonInput),
+  };
+};
+
+//procesar la entrada y mostrar resultado
+const showResult = (isValid) => {
+  console.log(isValid ? "Valid coordinates" : "Invalid coordinates");
+};
+
+const handleInput = (latInput, lonInput) => {
+  const { lat, lon } = convertToNumber(latInput, lonInput);
+
+  if (!areValidNumbers(lat, lon)) {
+    console.log("Please enter valid numbers.");
+  } else {
+    showResult(validCoordinates(lat, lon));
+  }
+  // se cierra la interfaz de readline
+  rl.close();
+};
+
+// Solo pide los datos
+const getCoordinates = () => {
+  rl.question("Enter latitude: ", (latInput) => {
+    rl.question("Enter longitude: ", (lonInput) => {
+      handleInput(latInput, lonInput);
     });
+  });
+};
 
-    
+//ejecución
+getCoordinates();
